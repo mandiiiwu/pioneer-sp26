c7638cb88c1fb48e26245dc1370d6d7d20af9f42
mandy wu <mandi.wu01@gmail.com>
2026-02-28 21:49:50 -0500

    milestone 1

---
+++ session1/milestone_1a.py
@@ -0,0 +1,18 @@
+import os
+import cv2
+
+current = os.getcwd() + '/session1/'
+
+img_names = [
+    'chicago.jpg',
+    'daylilies.jpg',
+    'astilbe.jpg',
+    'snow.jpg'
+]
+
+for name in img_names:
+    img = cv2.imread(f'{current}/SampleImages/{name}')
+    cv2.imshow(f'image', img)
+    cv2.waitKey(0)
+
+    
